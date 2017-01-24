424515922703810133d36947723504c3517000b8
Yehuda Sadeh <yehuda@redhat.com>
2017-01-24 15:18:50 -0800

    add virtualenv creation utils
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(
+    name='ragweed',
+    version='0.0.1',
+    packages=find_packages(),
+
+    author='Yehuda Sadeh',
+    author_email='yehuda@redhat.com',
+    description='A test suite for ceph rgw',
+    license='MIT',
+    keywords='ceph rgw testing',
+
+    install_requires=[
+        'boto >=2.0b4',
+        'PyYAML',
+        'bunch >=1.0.0',
+        'gevent >=1.0',
+        'isodate >=0.4.4',
+        ],
+
+    #entry_points={
+    #    'console_scripts': [
+    #        'ragweed = ragweed:main',
+    #        ],
+    #    },
+
+    )
